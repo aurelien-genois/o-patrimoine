280917b2ba04d0bc743d99a2960ffd61a23918b1
Aurélien Genois <aurelien.genois@akawam.com>
2022-09-12 23:50:59 +0200

    🚧 footer : legals mentions

--- themes/opatrimoine/footer.php
+++ themes/opatrimoine/footer.php
@@ -14,7 +14,7 @@ $adress = get_field('options_adress','option');
                 <?php foreach($socialsLinks as $socialsLink) : 
                     $iconUrl = wp_get_attachment_image_url($socialsLink['icon'],'medium', false);
                     ?>
-                    <a class="block w-11" href="<?= $socialsLink['link']['url'] ?>" target="<?= $socialsLink['link']['target'] ?>" title="<?= $socialsLink['link']['title'] ?>" alt="<?= $socialsLink['link']['title'] ?>">
+                    <a class="block w-11 hover:scale-110" href="<?= $socialsLink['link']['url'] ?>" target="<?= $socialsLink['link']['target'] ?>" title="<?= $socialsLink['link']['title'] ?>" alt="<?= $socialsLink['link']['title'] ?>">
                         <?php 
                             if(substr_count($iconUrl,'.svg')){
                                 echo file_get_contents( get_attached_file($socialsLink['icon']), false);
@@ -45,12 +45,18 @@ $adress = get_field('options_adress','option');
 
             
         <?php if(!empty($adress)) : ?>
-            <section class="w-full text-center md:w-1/3 order-3">
+            <section class="w-full text-center mb-4 md:w-1/3 order-3 md:mb-0">
                 <p class="text-main">
                     <?= $adress ?>
                 </p>
             </section>
         <?php endif; ?>
+
+        <section class="w-full flex justify-center flex-wrap space-y-2 md:space-x-2 md:space-y-0 order-4">
+            <a class="w-full md:w-auto text-center text-third visited:text-fourth hover:underline" href="<?= get_privacy_policy_url() ?>" title="Page de mentions légales" alt="Page de mentions légales">Mentions légales</a>
+            <p class="w-full md:w-auto text-center">©O'Patrimoine</p>
+            <p class="w-full md:w-auto text-center">Site réalisé par <a class="text-third visited:text-fourth hover:underline" href="https://wugenois.com/" target="_blank" title="Lien vers Wugenois.com" alt="Lien vers Wugenois.com">Aurélien Genois</a></p>
+        </section>
                                                               
     </footer>
 
