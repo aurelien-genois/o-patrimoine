bc4410f139c061e9acd6dbf0197d58ae2f26224b
Aurélien Genois <aurelien.genois@akawam.com>
2022-09-25 12:26:21 +0200

    :lipstick: smalls fixes

--- themes/opatrimoine/templates/lost_password.php
+++ themes/opatrimoine/templates/lost_password.php
@@ -7,11 +7,11 @@ the_post();
 ?>
 
 
-<div class="container px-6 md:px-8 lg:px-12 xl:px-18 2xl:px-28 mx-auto">
-    <h2 class="text-main"><?= the_title() ?></h2>
+<section class="container px-6 md:px-8 lg:px-12 xl:px-18 2xl:px-28 mx-auto">
+    <h2 class="titles text-center"><?= the_title() ?></h2>
 
-    <div class="text-center">TO DO</div>
-</div>
+    <div class="text-center">Page en construction</div>
+</section>
 
 
 <?php
